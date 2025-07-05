95a00e8d151a52ceb8f1b7758534587d67c9b35f
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-05 21:26:35 +0900

    postsテーブルからtitleカラム消去

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_05_103234) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_05_121154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
