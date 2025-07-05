595777600735a4d1cbfacdf5f91ea3411ebbf905
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-05 20:33:42 +0900

    ルーティング,Post#index,Postビュー追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :body, length: { maximum: 65_535 }
+  validates :restaurant_info, length: { maximum: 255 }
+  validates :food_info, length: { maximum: 255 }
+
+  belongs_to :user
 end
