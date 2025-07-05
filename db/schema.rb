7e3dbb65ca7947e7818628d2eddeae2c9c1ecfe4
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-05 19:52:41 +0900

    postsテーブルへカラムと外部キー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_04_085904) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_05_103234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_04_085904) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
+    t.string "restaurant_info"
+    t.string "food_info"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_04_085904) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
