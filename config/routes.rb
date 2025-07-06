9bc7d240f516505effb929b66d19b57920fd00f6
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-06 13:36:11 +0900

    route +Post#new +newビューまで実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users # deviseを使用するURLに「users」を含むということ
-  resources :posts, only: %i[ index ]
+  resources :posts, only: %i[ index new ]
   root "static_pages#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
