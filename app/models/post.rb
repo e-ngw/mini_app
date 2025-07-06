a459733cb08c71d52c20a701aaf66afe53aac18f
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-06 16:46:53 +0900

    Postモデルへ禁止ワードのバリ,バリデーションエラーをビュー表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,25 @@ class Post < ApplicationRecord
   validates :food_info, length: { maximum: 255 }
 
   belongs_to :user
+
+  FORBIDDEN_WORDS = %w[
+  まずい まずっ 不味 マズい マズイ まずすぎる 美味しくない おいしくない 食えない 食べられない もう食べたくない
+  最悪 ひどい 酷すぎ 失敗 不快 だめ 期待外れ 残念 がっかり ひどすぎ
+  くさい 臭い 変な味 腐ってる くさってる カビてる
+]
+
+  validate :body_cannot_include_forbidden_words
+
+  private
+
+  def body_cannot_include_forbidden_words
+    return if body.blank?
+
+    FORBIDDEN_WORDS.each do |word|
+      if body.include?(word)
+        errors.add(:body, ":「#{word}」を含む言葉は使用禁止です")
+        break
+      end
+    end
+  end
 end
