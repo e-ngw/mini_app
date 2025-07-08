4d5f92fb67f7873866c46e697155c45fd101f179
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-08 19:04:19 +0900

    Fix/Lintエラーのため

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = "fog/aws"
     config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                'ap-northeast-1' # 作成したバケットのリージョンと合わせる
+      provider:              "AWS",
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                "ap-northeast-1" # 作成したバケットのリージョンと合わせる
     }
-    config.fog_directory  = 'mini-app-image-bucket1' # あなたのバケット名
+    config.fog_directory  = "mini-app-image-bucket1" # あなたのバケット名
     config.fog_public     = true
     config.storage        = :fog
   else
