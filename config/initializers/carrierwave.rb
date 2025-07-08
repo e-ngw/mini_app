6c86a1e49de5491afbf70e449cef87d4157bbebe
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-08 20:25:15 +0900

    S3導入のため再設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,33 @@
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.fog_provider = "fog/aws"
+#     config.fog_credentials = {
+#       provider:              "AWS",
+#       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#       region:                "ap-northeast-1" # 作成したバケットのリージョンと合わせる
+#     }
+#     config.fog_directory  = "mini-app-image-bucket1" # あなたのバケット名
+#     config.fog_public     = true
+#     config.storage        = :fog
+#   else
+#     config.storage = :file
+#   end
+# end
+
+require "carrierwave/storage/abstract"
+require "carrierwave/storage/file"
+require "carrierwave/storage/fog"
+
 CarrierWave.configure do |config|
-  if Rails.env.production?
+    config.storage :fog
     config.fog_provider = "fog/aws"
-    config.fog_credentials = {
-      provider:              "AWS",
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region:                "ap-northeast-1" # 作成したバケットのリージョンと合わせる
-    }
     config.fog_directory  = "mini-app-image-bucket1" # あなたのバケット名
-    config.fog_public     = true
-    config.storage        = :fog
-  else
-    config.storage = :file
-  end
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: "ap-northeast-1", # リージョン
+      path_style: true
+    }
 end
