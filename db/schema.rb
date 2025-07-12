9c3e457579c32829987b01bfb36c5f5cf03f653a
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-12 22:41:33 +0900

    Followモデル生成しマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_12_085821) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_12_133157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
