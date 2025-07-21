52ffee1bb6f4d6d1869a4f8e65c55c90b11b373a
e-ngw <sa_ka321@yahoo.co.jp>
2025-07-21 16:55:45 +0900

    Uploder修正+ indexレイアウトにCSS Grid適用

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -41,14 +41,14 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # 画像の容量も小さくなる。一度だけ変換され、以降はその変換後の画像が使われる。
   # resize_to_fit は指定サイズに収まる最大サイズでアスペクト比（縦横比）を維持したまま縮小する
   # resize_to_fill は、中央をトリミングしながらサイズをピッタリ合わせる処理
-  process resize_to_limit: [ 1280, 1280 ] #オリジナルを容量軽く保存
+  process resize_to_limit: [ 1280, 1280 ] # オリジナルを容量軽く保存
 
   version :thumb do
-    process resize_to_fill: [ 480, 420 ]  # 一覧用サムネイル(8:7)
+    process resize_to_fill: [ 320, 320 ]  # 一覧用サムネイル
   end
 
   version :medium do
-    process resize_to_fit: [ 720, 540 ] # 詳細用(4:3)
+    process resize_to_fill: [ 720, 720 ] # 詳細用
   end
   # Process files as they are uploaded:
   # process scale: [200, 300]
