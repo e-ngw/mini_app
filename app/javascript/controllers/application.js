1b9782d7a62436c30daf305c1a98d20e760430b0
e-ngw <sa_ka321@yahoo.co.jp>
2025-08-12 20:41:40 +0900

    stimulus-autocomplete install+ jsへAutocompleteコントローラー登録

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,9 @@
+// Stimulusアプリ本体の生成＆コントローラ登録
 import { Application } from "@hotwired/stimulus"
+import { Autocomplete } from "stimulus-autocomplete" // 追加
 
 const application = Application.start()
+application.register("autocomplete", Autocomplete) // 追加
 
 // Configure Stimulus development experience
 application.debug = false
